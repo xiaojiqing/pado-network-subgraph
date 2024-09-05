3dbaa0a59289c449e57c51d748f242aeb0397036
xiaojiqing <xiaojiqing@padolabs.org>
2024-09-05 00:50:13 -0700

    worker counter

--- src/worker-mgt.ts
+++ src/worker-mgt.ts
@@ -3,8 +3,11 @@ import {
   WorkerRegistry as WorkerRegistryEvent
 } from "../generated/WorkerMgt/WorkerMgt"
 import {
-  WorkerInfo
+  WorkerInfo,
+  WorkerCounter
 } from "../generated/schema"
+
+const counterName = "WorkerCounter";
 
 export function handleWorkerRegistry(event: WorkerRegistryEvent): void {
     let entity = new WorkerInfo(event.params.workerId);
@@ -24,4 +27,18 @@ export function handleWorkerRegistry(event: WorkerRegistryEvent): void {
     entity.delegationAmount = worker.delegationAmount;
 
     entity.save();
+
+    // update worker count
+    increaseWorkerCount();
+}
+
+function increaseWorkerCount(): void {
+    let workerCounter = WorkerCounter.load(counterName);
+    if (workerCounter === null) {
+        workerCounter = new WorkerCounter(counterName);
+        workerCounter.workerCount = 0;
+    }
+
+    workerCounter.workerCount += 1;
+    workerCounter.save();
 }
