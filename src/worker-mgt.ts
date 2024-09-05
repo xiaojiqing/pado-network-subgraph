fc4e9c4b9ebcf693b8ef8174be6acace36a0089d
xiaojiqing <xiaojiqing@padolabs.org>
2024-09-05 00:23:51 -0700

    index worker

--- src/worker-mgt.ts
+++ src/worker-mgt.ts
@@ -1,70 +1,27 @@
 import {
-  Initialized as InitializedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  SelectDataWorkers as SelectDataWorkersEvent,
+  WorkerMgt,
   WorkerRegistry as WorkerRegistryEvent
 } from "../generated/WorkerMgt/WorkerMgt"
 import {
-  Initialized,
-  OwnershipTransferred,
-  SelectDataWorkers,
-  WorkerRegistry
+  WorkerInfo
 } from "../generated/schema"
 
-export function handleInitialized(event: InitializedEvent): void {
-  let entity = new Initialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleSelectDataWorkers(event: SelectDataWorkersEvent): void {
-  let entity = new SelectDataWorkers(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.dataId = event.params.dataId
-  entity.workerIds = event.params.workerIds
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
 export function handleWorkerRegistry(event: WorkerRegistryEvent): void {
-  let entity = new WorkerRegistry(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.workerId = event.params.workerId
-  entity.workerType = event.params.workerType
-  entity.owner = event.params.owner
+    let entity = new WorkerInfo(event.params.workerId);
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    let workerMgt = WorkerMgt.bind(event.address);
+    let worker = workerMgt.getWorkerById(event.params.workerId);
+    entity.workerType = worker.workerType;
+    entity.name = worker.name;
+    entity.desc = worker.desc;
+    entity.stakeAmount = worker.stakeAmount;
+    entity.owner = worker.owner;
+    entity.publicKey = worker.publicKey;
+    entity.time = worker.time;
+    entity.status = worker.status;
+    entity.sucTasksAmount = worker.sucTasksAmount;
+    entity.failTasksAmount = worker.failTasksAmount;
+    entity.delegationAmount = worker.delegationAmount;
 
-  entity.save()
+    entity.save();
 }
